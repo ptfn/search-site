80e34af9a99d1ec84554e922f60a43dba2efb097
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2020-09-30 14:38:26 +0700

    Fix Varrible

--- search_site.py
+++ search_site.py
@@ -7,17 +7,17 @@ Root = ['.com', '.ru', '.org', '.net']
 n = 0
 
 while n < 100:
-    Domen = ''
+    Domain = ''
     Url = ''
     n += 1
     
     file = open("site.txt", 'a+', encoding = 'utf-8')
 
     for i in range(Length):
-        Domen += random.choice(Symbols)
+        Domain += random.choice(Symbols)
 
     for i in range(len(Root)):
-        Url = 'https://' + Domen + Root[i]
+        Url = 'https://' + Domain + Root[i]
         try:      
             r = requests.get(Url, timeout = 10)
             if r.status_code in [200, 302, 304]:
