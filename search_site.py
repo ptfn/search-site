9e25261152c26ab92b206fd72a2b101733c99f54
ptfn <CipherDogs@protonmail.com>
2021-01-07 13:14:28 +0700

    Improved

--- search_site.py
+++ search_site.py
@@ -1,34 +1,37 @@
-import random
-import requests
+from random import randrange, choice
 from bs4 import BeautifulSoup
+import requests
 
-Symbols = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Length = random.randrange(1, 6)
+
+Symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Length = randrange(3, 10)
 Root = ['.com', '.ru', '.org', '.net']
-n = 0
 
-while n < 10:
+while 1:
     Domain = ''
     Url = ''
-    n += 1
 
     file = open("site.txt", 'a+', encoding='utf-8')
 
     for i in range(Length):
-        Domain += random.choice(Symbols)
+        Domain += choice(Symbols)
 
     for i in range(len(Root)):
-        url = 'https://' + Domain + Root[i]
+        url = 'http://' + Domain + Root[i]
+
         try:
             r = requests.get(url, timeout=5)
             soup = BeautifulSoup(r.content, 'html.parser')
             title = soup.title.string
+
             if r.status_code in [200, 302, 304]:
                 file.write('{}\t|\t{}\n'.format(url, title))
                 print(url)
+
             elif r.status_code in [502, 404, 403]:
                 print("Not found or not available")
+
         except:
-            print("Domain not exist")
+            print("Site not exist")
 
     file.close()
