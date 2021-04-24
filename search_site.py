e33be8d16b2253aeda333e105e465af550e3849c
ptfn <CipherDogs@protonmail.com>
2021-04-24 20:37:05 +0700

    fixed and upgrade code

--- search_site.py
+++ search_site.py
@@ -3,31 +3,61 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
+BigChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SmallChar = "abcdefghijklmnopqrstuvwxyz"
+Number = "0123456789"
+Symbols = "!?@#$%^&*=<>()[]/|,.+-_"
+
+
+def brut_alph(string):
+    str_last = ""
+    str_res = ""
+
+    for i in range(len(string)):
+        if string[i] not in str_last:
+            if string[i] == "b":
+                str_res += BigChar
+                str_last += "b"
+            if string[i] == "c":
+                str_res += SmallChar
+                str_last += "c"
+            if string[i] == "n":
+                str_res += Number
+                str_last += "n"
+            if string[i] == "s":
+                str_res += Symbols
+                str_last += "s"
+    return str_res
+
 
 def main():
     try:
         Max = int(sys.argv[1])
     except:
-        print("Enter password generation length example argument: python3 search_site.py 10  ")
-        exit(0)
-
+        if Max <= 1:
+            print("Argument must be greater than 1")
+            exit(0)
+        else:    
+            print("Enter password generation length example argument: python3 search_site.py 10 bcns")
+            exit(0)
+    
     try:
-        Length = randrange(1, Max)
+        Strings = sys.argv[2]
     except:
-        print("Argument must be greater than 1")
+        print("Enter password generation length example argument: python3 search_site.py 10 bcns")
         exit(0)
 
-    Symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     Root = ['.com', '.ru', '.org', '.net'] 
+    Alphabet = brut_alph(Strings)
 
     while 1:
         Domain = ''
         Url = ''
-
         file = open("site.txt", 'a+', encoding='utf-8')
+        Length = randrange(1, Max)
 
         for i in range(Length):
-            Domain += choice(Symbols)
+            Domain += choice(Alphabet)
 
         for i in range(len(Root)):
             Url = 'http://' + Domain + Root[i]
@@ -46,8 +76,8 @@ def main():
 
             except:
                 print("Site not exist")
-
-        file.close()
+            finally:
+                file.close()
 
 if __name__ == "__main__":
     main()
