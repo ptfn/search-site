b8148dfa231d614503799b551abd36445c58f46a
ptfn <CipherDogs@protonmail.com>
2021-10-20 18:15:03 +0700

    added statistic

--- search_site.py
+++ search_site.py
@@ -1,82 +1,100 @@
 from random import randrange, choice
 from bs4 import BeautifulSoup
 import requests, sys
+import time
 
-BigChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-SmallChar = "abcdefghijklmnopqrstuvwxyz"
-Number = "0123456789"
-Symbols = "!?@#$%^&*=<>()[]/|,.+-_"
+number = "0123456789"
+symbols = "!?@#$%^&*=<>()[]/|,.+-_"
+bigchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+smallchar = "abcdefghijklmnopqrstuvwxyz"
 
-
-def brut_alph(string):
+def brut(string):
     str_last = ""
     str_res = ""
 
     for i in range(len(string)):
         if string[i] not in str_last:
+
             if string[i] == "b":
-                str_res += BigChar
+                str_res += bigchar
                 str_last += "b"
+
             if string[i] == "c":
-                str_res += SmallChar
+                str_res += smallchar
                 str_last += "c"
+            
             if string[i] == "n":
-                str_res += Number
+                str_res += number
                 str_last += "n"
+            
             if string[i] == "s":
-                str_res += Symbols
+                str_res += symbols
                 str_last += "s"
+
     return str_res
 
 
 def main():
     try:
-        Max = int(sys.argv[1])
+        maximus = int(sys.argv[1])
     except:
-        if Max <= 1:
-            print("Argument must be greater than 1")
+        if maximus < 2 :
+            print("Argument must be greater than 1.")
             exit(0)
         else:    
-            print("Enter password generation length example argument: python3 search_site.py 10 bcns")
+            print("Enter password generation length example argument: python3 search_site.py 10 bcns.")
             exit(0)
     
     try:
-        Strings = sys.argv[2]
+        string = sys.argv[2]
     except:
-        print("Enter password generation length example argument: python3 search_site.py 10 bcns")
+        print("Enter password generation length example argument: python3 search_site.py 10 bcns.")
         exit(0)
 
-    Root = ['.com', '.ru', '.org', '.net'] 
-    Alphabet = brut_alph(Strings)
+    alphabet = brut(string)
+    root = ['.com', '.ru', '.org', '.net']
+    allsite = 0
+    found = 0
 
     while 1:
-        Domain = ''
-        Url = ''
-        file = open("site.txt", 'a+', encoding='utf-8')
-        Length = randrange(1, Max)
+        try:
+            url = ''
+            domain = ''
+            length = randrange(1, maximus)
+            file = open("site.txt", 'a+', encoding='utf-8')
 
-        for i in range(Length):
-            Domain += choice(Alphabet)
+            for i in range(length):
+                domain += choice(alphabet)
 
-        for i in range(len(Root)):
-            Url = 'http://' + Domain + Root[i]
+            for i in range(len(root)):
+                url = 'http://' + domain + root[i]
 
-            try:
-                r = requests.get(Url, timeout=2.5)
-                soup = BeautifulSoup(r.content, 'html.parser')
-                title = soup.title.string
+                try:
 
-                if r.status_code in [200, 302, 304]:
-                    file.write('{}\t|\t{}\n'.format(Url, title))
-                    print(Url)
+                    r = requests.get(url, timeout=10)
+                    soup = BeautifulSoup(r.content, 'html.parser')
+                    title = soup.title.string
 
-                elif r.status_code in [502, 404, 403]:
-                    print("Not found or not available")
+                    if r.status_code in [200, 302, 304]:
+                        file.write('{} - {}\n'.format(url, title))
+                        found += 1
+                        print(url)
 
-            except:
-                print("Site not exist")
-            finally:
-                file.close()
+                    elif r.status_code in [502, 404, 403]:
+                        print("Not found or not available!")
+
+                except:
+                    print("Site not exist!")
+
+                finally:
+                    allsite += 1
+                    file.close()
+                    time.sleep(0.5)
+
+        except KeyboardInterrupt:
+            print("\n--- search statistics ---")
+            print("all/found = {}/{}".format(allsite, found))
+            break
 
 if __name__ == "__main__":
     main()
